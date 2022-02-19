610e380fdbc31a4652af380d507c0235969c7efd
Baptiste Decarra <baptiste.decarra@gmail.com>
2022-02-19 12:55:02 +0100

    yeld app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Seeding..."
+
+res = Restaurant.new(name: "Epicurien", address: "1 rue carpe diem, Paris",category: "french")
+res.save
+puts Restaurant.last
+
+res = Restaurant.new(name: "Epicurien bis", address: "1 bis rue carpe diem, Paris",category: "french")
+res.save
+puts Restaurant.last
+
+res = Restaurant.new(name: "Spiagia di Oro", address:"1 plazza di Duomo, Milano" ,category: "italian")
+res.save
+puts Restaurant.last
+
+res = Restaurant.new(name: "Spiagia di Oro bis", address:"1 bis plazza di Duomo, Milano" ,category: "italian")
+res.save
+puts Restaurant.last
+
+res = Restaurant.new(name: "Taissei", address: "1 place edgar quinet, Paris", category: "japanese")
+res.save
+puts Restaurant.last
